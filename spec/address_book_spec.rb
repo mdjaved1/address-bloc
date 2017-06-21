c3b8459cab1963b3eafe6ca6925ff85ccc51ed37
Muhammad <daudjaved1@gmail.com>
2017-06-21 19:06:06 +0000

    reive aassign 2

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,5 +36,22 @@
        
      end
    end
+   describe "#remove_entry" do
+     it "removes only one entry from the address book" do
+         
+         book =AddressBook.new
+         book.add_entry('Mike far', '010.022.1815', 'a.king@lovelace.com')
+         
+         name = "Ada Lovelace"
+         phone_number = "010.012.1815"
+         email_address = "augusta.king@lovelace.com"
+         book.add_entry(name,phone_number,email_address)
+         
+         expect(book.entries.size).to eq(2)
+         book.remove_entry(name,phone_number,email_address)
+         expect(book.entries.size).to eq(1)
+         expect(book.entries.first.name).to eq("Mike far")
+         
+     end
+   end
  end
- 
