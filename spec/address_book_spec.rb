5e65a4a9d380bad084c1f8fc6e7a47e54722dca4
Muhammad <daudjaved1@gmail.com>
2017-06-24 03:41:19 +0000

    assign 5 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -84,6 +84,30 @@
        entry_five = book.entries[4]
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
+          it "imports the 1st entry from csv2" do
+       book.import_from_csv("entries_2.csv")
+       # Check the first entry
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+ 
+       # #5
+       expect(entry_one.name).to eq "Bill"
+       expect(entry_one.phone_number).to eq "555-555-4854"
+       expect(entry_one.email).to eq "bill@blocmail.com"
+     end
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+ 
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
    end
  
 end
