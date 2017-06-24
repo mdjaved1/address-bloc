60f5c4ccbed7891714d7176b7dbacccb54df2a71
Muhammad <daudjaved1@gmail.com>
2017-06-24 21:50:25 +0000

    checkpoint 6 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -2,7 +2,7 @@
  require_relative 'entry'
  require "csv"
  
-   class AddressBook
+ class AddressBook
      attr_reader :entries
 
      def initialize
@@ -31,4 +31,29 @@
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
     end
+    
+   def binary_search(name)
+     lower = 0
+     upper = entries.length - 1
+ 
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+ 
+     # #5
+     return nil
    end
+   
+ end
